bc19ca70984fc3beca0f943dac1b89af94341178
dbarros1979 <daniel.barros@genesis.global>
2023-03-07 13:19:54 +0000

    Model names as params

--- codet5_gpal.py
+++ codet5_gpal.py
@@ -1,9 +1,28 @@
 from transformers import AutoTokenizer, T5ForConditionalGeneration
 
-tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-large-ntp-py")
-model = T5ForConditionalGeneration.from_pretrained("Salesforce/codet5-large-ntp-py")
+hf_model_name = "icatlab-uiuc/codet5-normal"
+tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
+model = T5ForConditionalGeneration.from_pretrained(hf_model_name)
 
-text = "#define a function that says hello world in python\n"
+text = """
+    You are a helpful assistant that generates code in a proprietary language called GPAL.
+    This is how to create views in GPAL:
+    views {
+      view('TRADE_VIEW', TRADE) {
+        joins {
+          joining(INSTRUMENT) {
+            on(TRADE.INSTRUMENT_ID to INSTRUMENT { INSTRUMENT_ID })
+          }
+        }
+        fields {
+          TRADE.allFields()
+          INSTRUMENT.NAME withPrefix INSTRUMENT
+          INSTRUMENT.CURRENCY_ID withAlias 'CURRENCY'
+        }
+      }
+    }
+    #create a view using GPAL that joins TRADE and ALLOCATION tables where the join is TRADE_ID and everything from a allocation table should be returned as well as a TRADE_ID from TRADE table
+"""
 input_ids = tokenizer(text, return_tensors="pt").input_ids
 
 generated_ids = model.generate(input_ids, max_length=128)
--- codet5_python.py
+++ codet5_python.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, T5ForConditionalGeneration
 tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-large-ntp-py")
 model = T5ForConditionalGeneration.from_pretrained("Salesforce/codet5-large-ntp-py")
 
-text = "#define a function that says hello world in Kotlin\n"
+text = "#define a function that says hello world in python\n"
 input_ids = tokenizer(text, return_tensors="pt").input_ids
 
 generated_ids = model.generate(input_ids, max_length=128)
